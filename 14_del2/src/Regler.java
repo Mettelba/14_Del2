732f806901e9b4f8e2a1a6f526f11e4ac568c3b1
Rasmus <Rasmus@Rasmus-Pc.eduroam.wireless.dtu.dk>
2017-11-06 16:21:08 +0100

    Rasmus commit and push regler og spiller

---
+++ 14_del2/src/Regler.java
@@ -0,0 +1,110 @@
+public class Regler {
+
+
+	private int point; 
+	private boolean ekstratur;
+
+	
+	public Regler() {
+
+		this.ekstratur = false;
+
+	}
+
+	public int hentPoint() {
+
+		return this.point;
+
+	}
+
+
+	public void sætpoint(int point){
+
+		this.point = point;
+
+	}
+
+	public boolean hentEkstraTur() {
+
+		
+		return this.ekstratur;
+		
+
+
+	}
+
+	public void sætEkstraTur(boolean ekstratur) {
+
+
+		this.ekstratur = ekstratur;
+		
+
+	}
+
+	public void reglesæt (int terningslag) {  	//Her er reglerne for felterne som er givet i 
+												// kravspecifikationen.
+
+		switch (terningslag) {
+
+		case 2:
+
+			this.point = 250;
+			this.ekstratur = false;
+			break;
+
+		case 3:
+			this.point = -100;
+			this.ekstratur = false;
+			break;
+
+		case 4:
+			this.point = 100;
+			this.ekstratur = false;
+			break;
+
+		case 5:
+			this.point = -20;
+			this.ekstratur = false;
+			break;
+
+		case 6: 
+			this.point = 180;
+			this.ekstratur = false;
+			break;
+
+		case 7:
+			this.point = 0;
+			this.ekstratur = false;
+			break;
+
+		case 8: 
+			this.point = -70;
+			this.ekstratur = false;
+			break;
+
+		case 9:
+			this.point = 60;
+			this.ekstratur = false;
+			break;
+
+		case 10: 
+			this.point = -80;
+			this.ekstratur = true;
+			break;
+
+		case 11:
+			this.point = -50;
+			this.ekstratur = false;
+			break;
+
+		case 12:
+			this.point = 650;
+			this.ekstratur = false;
+			break;
+
+			
+
+		}
+
+	}
+}
---
+++ 14_del2/src/Spiller.java
@@ -0,0 +1,26 @@
+
+public class Spiller {
+	
+	private String navn = " ";
+	private Konto konto;
+	
+	public Spiller(String navn) {
+		
+		this.sætNavn(navn);
+		this.konto = new Konto();
+		
+		
+	}
+
+	public String hentNavn() {
+		return navn;
+	}
+
+	public void sætNavn(String navn) {
+		this.navn = navn;
+	}
+	
+	
+	
+
+}
