c29291af038448c1f2d7ed2d2daecdd9ca823076
KimSand <s@ndberg.dk>
2017-11-06 18:22:14 +0100

    Spil slut beskeder bliver skrevet på GUI istedet for i Konsol

--- 14_del2/src/game/Spil.java
+++ 14_del2/src/game/Spil.java
@@ -17,7 +17,7 @@ public class Spil {
 	
 	public static void main (String[] args) throws IOException{
 		Spil game = new Spil();
-		game.spil();
+		game.spil();// spil spil
 	}
  
 	private void spil() throws IOException {
