a505c62c6485f19b14d2dda2ed5f4589ad31e80d
Mettelba <s172840@student.dtu.dk>
2017-11-06 17:01:09 +0100

    fix dependencies - added Maven

---
+++ 14_del2/src/test/testTerning.java
@@ -0,0 +1,49 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class testTerning {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testTerning() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testKast() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testHentØjne() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSætØjne() {
+		fail("Not yet implemented");
+	}
+
+}
