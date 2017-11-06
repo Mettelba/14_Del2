635778e1dd6b88e5bec33dae4dceca3bcab16ffe
Mettelba <s172840@student.dtu.dk>
2017-11-06 17:21:05 +0100

    Ændringer i Raflebæger

--- 14_del2/src/game/Raflebæger.java
+++ 14_del2/src/game/Raflebæger.java
@@ -8,15 +8,13 @@ public class Raflebæger {
 	private int antalsider = 6; //6, repræsentere antal sider, kan skiftes eks antal 10 sider 
 	private Terning terning1;
 	private Terning terning2;
-	private int terning1værdi; 
-	//variable der indholder værdien for terning1, skal bruges til GUI
-	private int terning2værdi; 
-	//variable der indholder værdien for terning2, skal bruges til GUI
+
 	
-	
-	public Raflebæger() {
-		terning1 = new Terning(antalsider);
-		terning2 = new Terning(antalsider);
+	public Raflebæger(int antalsider) {
+		this.antalsider = antalsider;
+		terning1 = new Terning(this.antalsider);
+		terning2 = new Terning(this.antalsider);
+		//den kan modtage integer parameter og oprette to terninger med det antal sider
 	
 	}
 	public void ryst() { // ryst skal ikke returnere noget
@@ -25,23 +23,16 @@ public class Raflebæger {
 		terning1.kast();
 		terning2.kast();
 		
-		//
-		this.terning1værdi = terning1.hentØjne();
-		/*Denne terning1, hentes de øjne der lige er blevet kastet for terning1
-		og gemmer i variablen : terning1værdi*/
-		
-		this.terning2værdi = terning2.hentØjne();
-		/*Denne terning2, hentes de øjne der lige er blevet kastet for terning2
-		og gemmer i variablen : terning2værdi*/
 		
 	}
-	//en metode der henter værdien fra 
+	//en metode der henter værdien fra terning1 
 	public int hentTerning1værdi () {
-		return this.terning1værdi;
-	}
+		return terning1.hentØjne();
+		}
 	
+	//en metode der henter værdien fra terning1 
 	public int hentTerning2værdi () {
-		return this.terning2værdi;
+		return terning2.hentØjne();
 	}
 	
 	public int hentSum() { //parentes angiver parameter
